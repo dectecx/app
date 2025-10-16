45f8550c1f352c0c8bf092e65775781696f32a41
dectecx <75653667+dectecx@users.noreply.github.com>
2025-10-16 19:15:17 +0800

    ✨Integrate Serilog for enhanced logging capabilities. Updated appsettings.json for Serilog configuration, including minimum log levels and output sinks. Modified Program.cs to set up Serilog as the logging provider and added request logging middleware.

--- WebApplication1/WebApplication1/Program.cs
+++ WebApplication1/WebApplication1/Program.cs
@@ -8,135 +8,164 @@ using WebApplication1.Data;
 using WebApplication1.Handlers;
 using WebApplication1.Repositories;
 using WebApplication1.Services;
+using Serilog;
 
-var builder = WebApplication.CreateBuilder(args);
+// Configure Serilog for bootstrapping
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
 
-// Add CORS
-builder.Services.AddCors(options =>
+try
 {
-    options.AddPolicy("AllowSpecificOrigin",
-        policy =>
-        {
-            policy.WithOrigins("http://localhost:5175")
-                  .AllowAnyHeader()
-                  .AllowAnyMethod();
-        });
-});
+    Log.Information("Starting up the web host.");
 
-// Add services to the container.
-builder.Services.AddProblemDetails();
-builder.Services.AddExceptionHandler<InvalidCredentialsExceptionHandler>();
-builder.Services.AddExceptionHandler<UserAlreadyExistsExceptionHandler>();
-builder.Services.AddExceptionHandler<InvalidRefreshTokenExceptionHandler>();
-builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+    var builder = WebApplication.CreateBuilder(args);
 
+    // Replace the default logger with Serilog
+    builder.Host.UseSerilog((context, services, configuration) => configuration
+        .ReadFrom.Configuration(context.Configuration)
+        .ReadFrom.Services(services)
+        .Enrich.FromLogContext());
 
-// Add DbContext based on provider
-var provider = builder.Configuration.GetValue("DatabaseSettings:ProviderType", "SqlServer");
-
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-{
-    switch (provider)
+    // Add CORS
+    builder.Services.AddCors(options =>
     {
-        case "SqlServer":
-            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-            break;
-        case "InMemory":
-            options.UseInMemoryDatabase("WorkItemListApp");
-            break;
-        default:
-            throw new Exception($"Unsupported provider: {provider}");
-    }
-});
-
-// Add Repository
-builder.Services.AddScoped<IWorkItemRepository, WorkItemRepository>();
-builder.Services.AddScoped<IUserWorkItemStateRepository, UserWorkItemStateRepository>();
-
-// Add Services
-builder.Services.AddScoped<IAuthService, AuthService>();
-builder.Services.AddScoped<ITokenService, TokenService>();
-builder.Services.AddScoped<IWorkItemService, WorkItemService>();
-builder.Services.AddScoped<IUserStateService, UserStateService>();
-builder.Services.AddSingleton<ICacheService, MemoryCacheService>();
-
-// Add Caching
-builder.Services.AddMemoryCache();
-
-// Add FluentValidation
-builder.Services.AddControllers()
-    .AddFluentValidation(fv => fv.RegisterValidatorsFromAssemblyContaining<Program>());
-
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(options =>
-{
-    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-    {
-        In = ParameterLocation.Header,
-        Description = "Please enter a valid token",
-        Name = "Authorization",
-        Type = SecuritySchemeType.Http,
-        BearerFormat = "JWT",
-        Scheme = "Bearer"
-    });
-    options.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
-        {
-            new OpenApiSecurityScheme
+        options.AddPolicy("AllowSpecificOrigin",
+            policy =>
             {
-                Reference = new OpenApiReference
-                {
-                    Type = ReferenceType.SecurityScheme,
-                    Id = "Bearer"
-                }
-            },
-            new string[]{}
+                policy.WithOrigins("http://localhost:5175")
+                      .AllowAnyHeader()
+                      .AllowAnyMethod();
+            });
+    });
+
+    // Add services to the container.
+    builder.Services.AddProblemDetails();
+    builder.Services.AddExceptionHandler<InvalidCredentialsExceptionHandler>();
+    builder.Services.AddExceptionHandler<UserAlreadyExistsExceptionHandler>();
+    builder.Services.AddExceptionHandler<InvalidRefreshTokenExceptionHandler>();
+    builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+
+
+    // Add DbContext based on provider
+    var provider = builder.Configuration.GetValue("DatabaseSettings:ProviderType", "SqlServer");
+
+    builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    {
+        switch (provider)
+        {
+            case "SqlServer":
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+                break;
+            case "InMemory":
+                options.UseInMemoryDatabase("WorkItemListApp");
+                break;
+            default:
+                throw new Exception($"Unsupported provider: {provider}");
         }
     });
-});
 
-// Add Authentication
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
+    // Add Repository
+    builder.Services.AddScoped<IWorkItemRepository, WorkItemRepository>();
+    builder.Services.AddScoped<IUserWorkItemStateRepository, UserWorkItemStateRepository>();
+
+    // Add Services
+    builder.Services.AddScoped<IAuthService, AuthService>();
+    builder.Services.AddScoped<ITokenService, TokenService>();
+    builder.Services.AddScoped<IWorkItemService, WorkItemService>();
+    builder.Services.AddScoped<IUserStateService, UserStateService>();
+    builder.Services.AddSingleton<ICacheService, MemoryCacheService>();
+
+    // Add Caching
+    builder.Services.AddMemoryCache();
+
+    // Add FluentValidation
+    builder.Services.AddControllers()
+        .AddFluentValidation(fv => fv.RegisterValidatorsFromAssemblyContaining<Program>());
+
+
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen(options =>
     {
-        options.TokenValidationParameters = new TokenValidationParameters
+        options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
         {
-            ValidateIssuer = true,
-            ValidateAudience = true,
-            ValidateLifetime = true,
-            ValidateIssuerSigningKey = true,
-            ValidIssuer = builder.Configuration["Jwt:Issuer"],
-            ValidAudience = builder.Configuration["Jwt:Audience"],
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
-        };
+            In = ParameterLocation.Header,
+            Description = "Please enter a valid token",
+            Name = "Authorization",
+            Type = SecuritySchemeType.Http,
+            BearerFormat = "JWT",
+            Scheme = "Bearer"
+        });
+        options.AddSecurityRequirement(new OpenApiSecurityRequirement
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                },
+                new string[]{}
+            }
+        });
     });
 
+    // Add Authentication
+    builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+        .AddJwtBearer(options =>
+        {
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                ValidAudience = builder.Configuration["Jwt:Audience"],
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+            };
+        });
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
 
-// Configure the HTTP request pipeline.
-app.UseExceptionHandler();
+    var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    // Use Serilog for request logging
+    app.UseSerilogRequestLogging();
+
+    // Configure the HTTP request pipeline.
+    app.UseExceptionHandler();
+
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    // Use CORS policy
+    app.UseCors("AllowSpecificOrigin");
+
+    app.UseAuthentication();
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-
-// Use CORS policy
-app.UseCors("AllowSpecificOrigin");
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Host terminated unexpectedly.");
+}
+finally
+{
+    Log.Information("Shutting down the web host.");
+    Log.CloseAndFlush();
+}
